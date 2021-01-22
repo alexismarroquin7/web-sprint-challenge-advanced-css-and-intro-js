03506c667009a224c18c514bdb87e2f504b9e82c
Alexis Marroquin <alexismarroquin754@gmail.com>
2021-01-22 10:01:54 -0800

    js task 4

--- index.js
+++ index.js
@@ -245,11 +245,18 @@ Use get20s to do the following:
 Example born in 1901 and died in 1959 - included -- born in 1889 and died in 1925 not included
 If correct, the function should return ["Salvador Dali", "Frida Kahlo"]*/
 
-function get20s(/*Your Code Here*/){
+function get20s(array){
   /*Your Code Here*/
+  let newArray = [];
+  for(let i = 0; i < array.length; i++){
+    if(array[i].years.includes('190')){
+      newArray.push(array[i].name);
+    }
+  }
+  return newArray;
 }
 
-
+console.log(get20s(artists));
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 5: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
  Use removeArtist to do the following:
